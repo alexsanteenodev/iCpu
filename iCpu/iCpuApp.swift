dd342455fdb711e2808fe1c92fc6119aaed7c4f9
Alex Santeeno <alexsanteeno@gmail.com>
2023-02-18 18:13:18 +0100

    Initial Commit

---
+++ iCpu/iCpuApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCpuApp.swift
+//  iCpu
+//
+//  Created by Oleksandr Hanhaliuk on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct iCpuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
